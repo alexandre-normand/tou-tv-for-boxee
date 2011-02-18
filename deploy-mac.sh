62eaa6e0d0d3058df913d5cc5b159969cf10475e
alexandre.normand <alexandre.normand@76950440-ca54-dd89-4f63-e7ba4ee03d0b>
2011-02-18 03:31:57 +0000

    Started work on new user interface.

---
+++ deploy-mac.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /Volumes/Bubble/projects/tou-tv-for-boxee
+rm -r ~/Library/Application\ Support/BOXEE/UserData/apps/com.googlecode.tou-tv-for-boxee.tou-tv
+svn export com.googlecode.tou-tv-for-boxee.tou-tv ~/Library/Application\ Support/BOXEE/UserData/apps/com.googlecode.tou-tv-for-boxee.tou-tv
+cp com.googlecode.tou-tv-for-boxee.tou-tv/skin/Boxee\ Skin\ NG/media/*.png ~/Library/Application\ Support/BOXEE/UserData/apps/com.googlecode.tou-tv-for-boxee.tou-tv/skin/Boxee\ Skin\ NG/media/
+sed -i.bak 's/<\/app>/<test-app>true<\/test-app><\/app>/g' ~/Library/Application\ Support/BOXEE/UserData/apps/com.googlecode.tou-tv-for-boxee.tou-tv/descriptor.xml
