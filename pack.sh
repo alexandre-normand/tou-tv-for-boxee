1caa443a1b538ea7fc5c9ea626d2d542c267b36b
damien.dube <damien.dube@76950440-ca54-dd89-4f63-e7ba4ee03d0b>
2011-02-19 16:12:16 +0000

    try a new packaging methode, based on the 1.6 package

--- pack.sh
+++ pack.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/bin/bash -x
 
 VERSION=`cat com.googlecode.tou-tv-for-boxee.tou-tv/descriptor.xml | grep "<version>" | sed -e 's/ //g' -e 's/<version>//' -e 's/<\/version>//'`
 
-mkdir packaging
+# Delete previous version
+rm -f boxee/download/com.googlecode.tou-tv-for-boxee.tou-tv-$VERSION.zip
+
+# Create packaging folder
+svn export com.googlecode.tou-tv-for-boxee.tou-tv packaging
+
 cd packaging
 
+#Export files in folder
 svn export ../com.googlecode.tou-tv-for-boxee.tou-tv com.googlecode.tou-tv-for-boxee.tou-tv
 
-zip -r ../boxee/download/com.googlecode.tou-tv-for-boxee.tou-tv-$VERSION.zip -xi com.googlecode.tou-tv-for-boxee.tou-tv
+
+# Zip files
+zip -r ../boxee/download/com.googlecode.tou-tv-for-boxee.tou-tv-$VERSION.zip -xi .
 
 cd -
 rm -rf packaging
+
+exit 0
